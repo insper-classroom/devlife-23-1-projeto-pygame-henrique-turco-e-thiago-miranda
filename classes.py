6fb4507213ed41e73d86467274b365915783af72
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-04-20 15:07:02 -0300

    Desenho dos círculos na tela

--- classes.py
+++ classes.py
@@ -6,49 +6,23 @@ cores = [
     (0, 255, 0),
     (255, 0, 0)
 ]
-posicoes = [
-    (340, 270),
-    (560, 270),
-    (340, 450),
-    (560, 450)
-]
-class Circulo:
-    def __init__(self, x, y, raio):
-        self.x = x
-        self.y = y
-        self.raio = raio
-        self.lista_cores = cores
-        self.lista_posicoes = posicoes
-        self.circulos = []
-    
-    # def desenha(self, window):
-    #     for i in range(len(self.lista_cores)):
-    #         self.circulos.append((window, self.lista_cores[i], (self.lista_posicoes[i]), self.raio))
-    
-    def verifica_clique(self, x, y):
-        self.x = x
-        self.y = y
-        self.distancia_ponto_centro = ((self.cx - self.x)**2 + (self.cy - self.y)**2)**0.5
-        if self.distancia_ponto_centro <= self.raio:
-            return True
-        else:    
-            return False
 
-class Jogo(Circulo):
+class Jogo:
     def __init__(self):
         pygame.init()
-        self.largura_janela = 900
+        self.largura_janela = 1000
         self.altura_janela = 600
         self.lista_cores = cores
-        self.lista_posicoes = posicoes
+        
         self.raio = 55
         self.window = pygame.display.set_mode((self.largura_janela, self.altura_janela))
         pygame.display.set_caption('Pattern Pursuit')
+        self.circulos = Circulo(self.window)
 
     def desenha(self, window):
         self.window.fill((0, 0, 0))
-        for i in range(len(self.lista_cores)):
-            pygame.draw.circle(window, self.lista_cores[i], self.lista_posicoes[i], self.raio)
+        self.circulos.desenha()
+
         pygame.display.update()
     
     def atualiza_estado(self):
@@ -60,3 +34,23 @@ class Jogo(Circulo):
     def game_loop(self):
         while self.atualiza_estado():
             self.desenha(self.window)
+
+class Circulo:
+    def __init__(self, window):
+        self.window = window
+        self.posicoes = [
+        (340, 270),
+        (560, 270),
+        (340, 450),
+        (560, 450)
+        ]
+        self.lista_cores = cores
+        self.raio = 60
+        self.circulos = []
+        
+        for i in range(len(self.lista_cores)):
+            self.circulos.append((window, self.lista_cores[i], (self.posicoes[i]), self.raio))
+    
+    def desenha(self):
+        for i in range(len(self.lista_cores)):
+            pygame.draw.circle(self.window, self.lista_cores[i], self.posicoes[i], self.raio)
