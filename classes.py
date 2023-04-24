622364eb817489698433acb2c6676362acc29a93
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-04-24 16:17:45 -0300

    Verificando cliques, mas sem validação de jogada

--- classes.py
+++ classes.py
@@ -11,32 +11,71 @@ cores = [
 class Jogo:
     def __init__(self):
         pygame.init()
-        self.largura_janela = 1000
+        self.largura_janela = 900
         self.altura_janela = 600
         self.lista_cores = cores
         
-        self.raio = 55
         self.window = pygame.display.set_mode((self.largura_janela, self.altura_janela))
         pygame.display.set_caption('Pattern Pursuit')
         self.circulos = Circulo(self.window)
+        self.retangulo = self.circulos.retangulo
+        self.jogadas_player = []
+        self.rect_sorteados = self.circulos.rect_sorteados
+        self.validacao = True
+        self.cores_sorteadas = self.circulos.cores_sorteadas
+        self.rodada = 1
+
 
     def desenha(self, window):
         self.window.fill((0, 0, 0))
         self.circulos.desenha()
-        if self.circulos.verifica_clique():
-            self.circulos.sorteia_cores()
-
+        for i in range(self.rodada):
+            if self.validacao:
+                self.circulos.sorteia_circulos()
         pygame.display.update()
     
     def atualiza_estado(self):
         for event in pygame.event.get():
+            self.x, self.y = pygame.mouse.get_pos()
+            mouse_rect = pygame.Rect(self.x, self.y, 1, 1)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print('chegou')
+                if mouse_rect.colliderect(self.retangulo[0]):
+                    
+                    self.jogadas_player.append(self.lista_cores[0])
+                
+                elif mouse_rect.colliderect(self.retangulo[1]):
+                    
+                    self.jogadas_player.append(self.lista_cores[1])
+                
+                elif mouse_rect.colliderect(self.retangulo[2]):
+                    
+                    self.jogadas_player.append(self.lista_cores[2])
+                
+                elif mouse_rect.colliderect(self.retangulo[3]):
+                    
+                    self.jogadas_player.append(self.lista_cores[3])
+                print(self.jogadas_player)
+                print(self.cores_sorteadas)
+                
+                if self.jogadas_player == self.cores_sorteadas:
+                    print('validou')
+                    self.rodada+=1
+                    self.validacao = True
+            
+            self.jogadas_player = []
+            
             if event.type == pygame.QUIT:
                 return False
         return True
     
+
+    
     def game_loop(self):
         while self.atualiza_estado():
             self.desenha(self.window)
+
 
 class Circulo:
     def __init__(self, window):
@@ -55,37 +94,33 @@ class Circulo:
             self.circulos.append((window, self.lista_cores[i], (self.posicoes[i]), self.raio))
         
         self.cores_sorteadas = []
-        self.jogadas_player = []
+        self.rect_sorteados = []
+        self.retangulo = []
     
     def desenha(self):
         for i in range(len(self.lista_cores)):
             pygame.draw.circle(self.window, self.lista_cores[i], self.posicoes[i], self.raio)
+            r = pygame.Rect(self.posicoes[i][0] - self.raio, self.posicoes[i][1] - self.raio, self.raio*2, self.raio*2)
+            self.retangulo.append(r) 
     
-    def verifica_clique(self):
-        self.x, self.y = pygame.mouse.get_pos()
-        self.distancia_ponto_centro = ((self.x - self.posicoes[0][0])**2 + (self.y - self.posicoes[0][1])**2)**(1/2)
+    def sorteia_circulos(self):
+        self.tempo = pygame.time.get_ticks() // 1000
+        self.tempo_anterior = 0
+        circulo_sorteado = random.choice(self.circulos)
+        self.cores_sorteadas.append(circulo_sorteado[1])
+        self.rect_sorteados.append(pygame.Rect(circulo_sorteado[2][0]-self.raio, circulo_sorteado[2][1]-self.raio, self.raio*2, self.raio*2))
         
-        if self.distancia_ponto_centro <= self.raio:
+        for cor in self.cores_sorteadas:
             for circulos in self.circulos:
-                if self.x == circulos[2][0] and self.y == circulos[2][1] and pygame.MOUSEBUTTONDOWN:
-                    self.jogadas_player.append(circulos[1])
+                    if cor == circulos[1]:
+                        if self.tempo - self.tempo_anterior == 1:
+                            print('entrou')
+                            pygame.draw.circle(self.window, (0, 0, 0), circulos[2], self.raio)
+                            pygame.display.update()
+                            self.tempo_anterior = self.tempo
+                            
         
-        if self.jogadas_player == self.cores_sorteadas:
-            return True
-
-    def sorteia_cores(self):
-            
-            cor_sorteada = random.choice(self.lista_cores)
-            self.cores_sorteadas.append(cor_sorteada)
-        
-            for circulos in self.circulos:
-                if circulos[1] == cor_sorteada:
-                    pygame.draw.circle(self.window, cor_sorteada, circulos[2], self.raio)
-                    pygame.display.update()
-                    pygame.time.wait(1000)
-                    pygame.draw.circle(self.window, (0, 0, 0), circulos[2], self.raio)
-                    pygame.display.update()
-                    pygame.time.wait(1000)
+                        
 
 
 
