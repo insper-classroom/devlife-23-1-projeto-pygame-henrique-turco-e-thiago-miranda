5300e8b437a19c3ad30eebfc022728607152d28b
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-04-25 18:01:07 -0300

    Modo clássico funcionando

--- classes.py
+++ classes.py
@@ -24,23 +24,27 @@ class Jogo:
         self.validacao = True
         self.cores_sorteadas = self.circulos.cores_sorteadas
         self.rodada = 1
+        
 
 
     def desenha(self, window):
         self.window.fill((0, 0, 0))
         self.circulos.desenha()
-        for i in range(self.rodada):
-            if self.validacao:
-                self.circulos.sorteia_circulos()
+        
         pygame.display.update()
     
     def atualiza_estado(self):
+        self.circulos.atualiza()
+        if self.validacao:
+            self.circulos.sorteia_circulos()
+            self.validacao = False
+        
         for event in pygame.event.get():
             self.x, self.y = pygame.mouse.get_pos()
             mouse_rect = pygame.Rect(self.x, self.y, 1, 1)
             
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print('chegou')
+            if event.type == pygame.MOUSEBUTTONUP:
+                
                 if mouse_rect.colliderect(self.retangulo[0]):
                     
                     self.jogadas_player.append(self.lista_cores[0])
@@ -56,15 +60,17 @@ class Jogo:
                 elif mouse_rect.colliderect(self.retangulo[3]):
                     
                     self.jogadas_player.append(self.lista_cores[3])
+                
                 print(self.jogadas_player)
                 print(self.cores_sorteadas)
                 
                 if self.jogadas_player == self.cores_sorteadas:
                     print('validou')
-                    self.rodada+=1
+                    self.circulos.indice_circulo = 0
                     self.validacao = True
             
-            self.jogadas_player = []
+            if len(self.jogadas_player) == len(self.cores_sorteadas):
+                self.jogadas_player = []
             
             if event.type == pygame.QUIT:
                 return False
@@ -96,28 +102,61 @@ class Circulo:
         self.cores_sorteadas = []
         self.rect_sorteados = []
         self.retangulo = []
-    
-    def desenha(self):
-        for i in range(len(self.lista_cores)):
-            pygame.draw.circle(self.window, self.lista_cores[i], self.posicoes[i], self.raio)
-            r = pygame.Rect(self.posicoes[i][0] - self.raio, self.posicoes[i][1] - self.raio, self.raio*2, self.raio*2)
-            self.retangulo.append(r) 
+        self.indice_circulo = 0
+        self.tempo_start = pygame.time.get_ticks()
+        self.mostra_circulo = True
     
     def sorteia_circulos(self):
-        self.tempo = pygame.time.get_ticks() // 1000
-        self.tempo_anterior = 0
         circulo_sorteado = random.choice(self.circulos)
         self.cores_sorteadas.append(circulo_sorteado[1])
         self.rect_sorteados.append(pygame.Rect(circulo_sorteado[2][0]-self.raio, circulo_sorteado[2][1]-self.raio, self.raio*2, self.raio*2))
+    
+    def desenha(self):
         
-        for cor in self.cores_sorteadas:
-            for circulos in self.circulos:
-                    if cor == circulos[1]:
-                        if self.tempo - self.tempo_anterior == 1:
-                            print('entrou')
-                            pygame.draw.circle(self.window, (0, 0, 0), circulos[2], self.raio)
-                            pygame.display.update()
-                            self.tempo_anterior = self.tempo
+        for i in range(len(self.lista_cores)):
+            r = pygame.Rect(self.posicoes[i][0] - self.raio, self.posicoes[i][1] - self.raio, self.raio*2, self.raio*2)
+            self.retangulo.append(r) 
+    
+            if self.mostra_circulo or self.indice_circulo >= len(self.cores_sorteadas) or self.lista_cores[i] != self.cores_sorteadas[self.indice_circulo]:
+                pygame.draw.circle(self.window, self.lista_cores[i], self.posicoes[i], self.raio)
+            
+    
+            
+    
+    def atualiza(self):
+        
+        self.tempo_passado = pygame.time.get_ticks() - self.tempo_start
+        print(self.tempo_passado)
+            
+        if self.tempo_passado > 1000:
+            self.mostra_circulo = not self.mostra_circulo
+            self.tempo_start = pygame.time.get_ticks()
+            if self.mostra_circulo:
+                self.indice_circulo+=1
+        
+                
+            
+        
+        
+        
+        # # for cor in self.cores_sorteadas:   
+        #    for circulos in self.circulos:     
+        #         if circulos[1] == cor:
+                
+        #             pygame.draw.circle(self.window, (0, 0, 0), circulos[2], self.raio)
+        #             pygame.display.update()
+                    
+                    
+        #             pygame.draw.circle(self.window, cor, circulos[2], self.raio)
+        #             pygame.display.update()
+                    
+                    
+                       
+
+                
+                            
+        
+       
                             
         
                         
